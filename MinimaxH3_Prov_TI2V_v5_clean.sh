604d5ec60246b1eeb424fb551cabdf94d5003198
thegriffinfamily3314 <thegriffinfamily3314@proton.me>
2026-08-25 16:39:12 -0300

    Update MinimaxH3_Prov_TI2V_v5_clean.sh

--- MinimaxH3_Prov_TI2V_v5_clean.sh
+++ MinimaxH3_Prov_TI2V_v5_clean.sh
@@ -874,11 +874,7 @@ WF="${COMFY}/user/default/workflows"
 
 # --- Civitai:  dest_dir | dest_filename | full_url ---
 CIVITAI_FILES=(
-    "$LORA|H3_Epic_Cumshots.safetensors|https://civitai.red/api/download/models/3202064?fileId=3083352"
-    "$LORA|H3_HMNSFW_AIO_Sex_v2.safetensors|https://civitai.red/api/download/models/3206518?fileId=3088013"
-    "$LORA|H3_Mini_Dick_Fix.safetensors|https://civitai.red/api/download/models/3207332?fileId=3088892"
-    "$LORA|H3_K3NK_Side_View_Deepthroat.safetensors|https://civitai.red/api/download/models/3216591?fileId=3098396"
-    "$LORA|H3_blowjob.safetensors|https://civitai.red/api/download/models/3235946?fileId=3118341"
+    
 )
 
 # --- Hugging Face:  hf | dest_dir | dest_filename | repo_id | repo_path ---
